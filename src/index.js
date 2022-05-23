75efbeacce4b9bdb2b41deb66dbb37e5239c17b7
Tatianayarchenko <ktanya61@gmail.com>
2022-05-23 11:46:25 +0300

    видалила папку src та додала з 10го проекту

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-import './sass/main.scss';
+import './css/styles.css';
+
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import countriesItem from './templates/countries-item';
+import countryItem from './templates/country-items';
+
+const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  searchBox: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.searchBox.addEventListener('input', debounce(onInputSeach, DEBOUNCE_DELAY));
+
+function clearInput() {
+  refs.countryList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
+}
+
+function onInputSeach(e) {
+  const inputValue = e.target.value.trim();
+
+  if (inputValue === '') {
+    clearInput();
+    return;
+  }
+  fetchCountries(inputValue)
+    .then(country => {
+      if (country.length > 10) {
+        clearInput();
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+      } else if (country.length === 1) {
+        clearInput();
+        renderCountry(country);
+      } else if (country.length >= 2 || country.length <= 10) {
+        clearInput();
+        renderCountries(country);
+      }
+    })
+    .catch(error => {
+      clearInput();
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
+}
+
+function renderCountries(countrys) {
+  const markup = countrys.map(countriesItem).join('');
+  refs.countryList.insertAdjacentHTML('afterbegin', markup);
+}
+
+function renderCountry(country) {
+  const markup = country.map(countryItem).join('');
+  refs.countryInfo.insertAdjacentHTML('afterbegin', markup);
+}
