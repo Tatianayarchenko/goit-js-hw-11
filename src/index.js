40ab36e6b3b500d4a3c10bdd9131b000899f2edd
Tatianayarchenko <ktanya61@gmail.com>
2022-05-24 00:09:40 +0300

    получила данніе с бекенда и зарендерила разметку

--- src/index.js
+++ src/index.js
@@ -1,58 +1,157 @@
-import './css/styles.css';
-
-import { fetchCountries } from './fetchCountries';
-import debounce from 'lodash.debounce';
+import axios from 'axios';
 import Notiflix from 'notiflix';
-import countriesItem from './templates/countries-item';
-import countryItem from './templates/country-items';
-
-const DEBOUNCE_DELAY = 300;
+import images from './templates/images';
 
 const refs = {
-  searchBox: document.querySelector('#search-box'),
-  countryList: document.querySelector('.country-list'),
-  countryInfo: document.querySelector('.country-info'),
+  form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
 };
 
-refs.searchBox.addEventListener('input', debounce(onInputSeach, DEBOUNCE_DELAY));
+refs.form.addEventListener('submit', onFormSubmit);
 
-function clearInput() {
-  refs.countryList.innerHTML = '';
-  refs.countryInfo.innerHTML = '';
+function onFormSubmit(e) {
+  e.preventDefault();
+  // const delay = Number(form.elements.delay.value);
+  const inputValue = refs.form.elements.searchQuery.value;
+  console.log(inputValue);
+  // fetch(
+  //   `https://pixabay.com/api/?key=27599819-5f2242c0de29668fb10ee249b&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`,
+  // )
+  //   .then(response => response.json())
+  //   .then(value => {
+  //     console.log(value);
+  //   });
+
+  axios(
+    `https://pixabay.com/api/?key=27599819-5f2242c0de29668fb10ee249b&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`,
+  ).then(resalt => {
+    const data = resalt.data.hits;
+    console.log(data);
+    if (data.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.',
+      );
+    }
+
+    renderGalery(data);
+  });
 }
 
-function onInputSeach(e) {
-  const inputValue = e.target.value.trim();
-
-  if (inputValue === '') {
-    clearInput();
-    return;
-  }
-  fetchCountries(inputValue)
-    .then(country => {
-      if (country.length > 10) {
-        clearInput();
-        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-      } else if (country.length === 1) {
-        clearInput();
-        renderCountry(country);
-      } else if (country.length >= 2 || country.length <= 10) {
-        clearInput();
-        renderCountries(country);
-      }
-    })
-    .catch(error => {
-      clearInput();
-      Notiflix.Notify.failure('Oops, there is no country with that name');
-    });
+function renderGalery(data) {
+  const markup = data.map(images).join('');
+  refs.gallery.insertAdjacentHTML('afterbegin', markup);
 }
 
-function renderCountries(countrys) {
-  const markup = countrys.map(countriesItem).join('');
-  refs.countryList.insertAdjacentHTML('afterbegin', markup);
-}
+// ==============================================
+// <!-- другое решение -->
 
-function renderCountry(country) {
-  const markup = country.map(countryItem).join('');
-  refs.countryInfo.insertAdjacentHTML('afterbegin', markup);
-}
+// import Notiflix from 'notiflix';
+// import imgTemplate from './templates/image.hbs';
+// import fetchApiByName from './pixabay-service';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// const API = new fetchApiByName();
+// const lightbox = new SimpleLightbox('.photo-link', {
+//   overlayOpacity: 0.4,
+//   animationSpeed: 100,
+// });
+
+// const refs = {
+//   form: document.querySelector('.search-form'),
+//   gallery: document.querySelector('.gallery'),
+//   showMoreBtn: document.querySelector('.load-more '),
+// };
+
+// refs.form.addEventListener('submit', onSearchForm);
+// refs.showMoreBtn.addEventListener('click', onShowMore);
+
+// async function onSearchForm(e) {
+//   e.preventDefault();
+
+//   const inputValue = e.currentTarget.elements.searchQuery.value;
+
+//   if (inputValue === '') {
+//     chekInputNotEmpty();
+//     return;
+//   }
+
+//   API.resetPage();
+//   API.queryValue(inputValue);
+
+//   try {
+//     const result = await API.fetchDataFromPixabay();
+
+//     resetMarkup();
+//     rewrightMarkup(result);
+
+//     API.setTotalHits(result.data.totalHits);
+//     lightbox.refresh();
+//     Notiflix.Notify.success(`Hooray! We found ${API.totalHits} images.`);
+
+//     showShowMoreBtn();
+//   } catch (error) {
+//     Notiflix.Notify.failure(
+//       'Sorry, there are no images matching your search query. Please try again.',
+//     );
+//   }
+// }
+
+// async function onShowMore() {
+//   API.increasePage();
+
+//   if (API.totalHits <= 20) {
+//     chekEndOfTotalHits();
+//     return;
+//   }
+
+//   const result = await API.fetchDataFromPixabay();
+
+//   rewrightMarkup(result);
+//   scrollAfterShowMore();
+
+//   API.lastTotalHits();
+//   lightbox.refresh();
+//   Notiflix.Notify.success(`Hooray! We found ${API.totalHits} images.`);
+// }
+
+// function rewrightMarkup(markup) {
+//   refs.gallery.insertAdjacentHTML('beforeend', imgTemplate(markup));
+// }
+
+// function resetMarkup() {
+//   refs.gallery.innerHTML = '';
+// }
+
+// function hideShowMoreBtn() {
+//   refs.showMoreBtn.classList.add('is-hidden');
+// }
+
+// function showShowMoreBtn() {
+//   refs.showMoreBtn.classList.remove('is-hidden');
+// }
+
+// function chekInputNotEmpty() {
+//   Notiflix.Notify.failure('Please enter something in search field');
+
+//   hideShowMoreBtn();
+//   resetMarkup();
+// }
+
+// function chekEndOfTotalHits() {
+//   hideShowMoreBtn();
+//   Notiflix.Notify.info("We're sorry, but you've reached the end of search results");
+// }
+
+// function scrollAfterShowMore() {
+//   let verticalParams = 0;
+
+//   const intervalId = setInterval(() => {
+//     window.scrollBy(0, verticalParams);
+//     verticalParams += 1;
+
+//     if (verticalParams === 20) {
+//       clearInterval(intervalId);
+//     }
+//   }, 20);
+// }
