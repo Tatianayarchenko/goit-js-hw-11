a17b6a4d69c1e066a714c4b764aacf682ab620e8
Tatianayarchenko <ktanya61@gmail.com>
2022-05-29 10:59:45 +0300

    подчистила коментарии

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import images from './templates/images';
 import PixabayService from './pixabay-service';
 import Notiflix from 'notiflix';
 import LoadMoreButton from './components/load_more_btn';
-
 import SimpleLightbox from 'simplelightbox';
-// import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
   form: document.querySelector('#search-form'),
@@ -15,13 +12,7 @@ const refs = {
 const pixabayService = new PixabayService();
 const loadMoreButton = new LoadMoreButton({ selector: '.load-more', hidden: true });
 
-const simpleLightbox = new SimpleLightbox('.image-link', {
-  // captionPosition: 'bottom',
-  // captionsData: 'alt',
-  // captionDelay: 250,
-  // overlayOpacity: 0.4,
-  // animationSpeed: 100,
-});
+const simpleLightbox = new SimpleLightbox('.image-link');
 
 refs.form.addEventListener('submit', onSearch);
 loadMoreButton.button.addEventListener('click', onButtonLoadMoreClick);
@@ -80,133 +71,3 @@ function clearGalery() {
   refs.gallery.innerHTML = '';
   loadMoreButton.hide();
 }
-// ==============================================
-//
-// ==============================================
-
-// loadBtn.button.addEventListener('click', onButtonLoadMoreClick);
-// const loadBtn = {
-//   button: document.querySelector('.load-more'),
-//   show() {
-//     this.button.classList.remove('is-hidden');
-//     console.log('кнопка появилась');
-//   },
-//   hide() {
-//     this.button.classList.add('is-hidden');
-//     console.log('кнопка спряталась');
-//   },
-// };
-// loadBtn.hide();
-// ==============================================
-// <!-- другое решение -->
-
-// import Notiflix from 'notiflix';
-// import imgTemplate from './templates/image.hbs';
-// import fetchApiByName from './pixabay-service';
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-
-// const API = new fetchApiByName();
-// const lightbox = new SimpleLightbox('.photo-link', {
-//   overlayOpacity: 0.4,
-//   animationSpeed: 100,
-// });
-
-// const refs = {
-//   form: document.querySelector('.search-form'),
-//   gallery: document.querySelector('.gallery'),
-//   showMoreBtn: document.querySelector('.load-more '),
-// };
-
-// refs.form.addEventListener('submit', onSearchForm);
-// refs.showMoreBtn.addEventListener('click', onShowMore);
-
-// async function onSearchForm(e) {
-//   e.preventDefault();
-
-//   const inputValue = e.currentTarget.elements.searchQuery.value;
-
-//   if (inputValue === '') {
-//     chekInputNotEmpty();
-//     return;
-//   }
-
-//   API.resetPage();
-//   API.queryValue(inputValue);
-
-//   try {
-//     const result = await API.fetchDataFromPixabay();
-
-//     resetMarkup();
-//     rewrightMarkup(result);
-
-//     API.setTotalHits(result.data.totalHits);
-//     lightbox.refresh();
-//     Notiflix.Notify.success(`Hooray! We found ${API.totalHits} images.`);
-
-//     showShowMoreBtn();
-//   } catch (error) {
-//     Notiflix.Notify.failure(
-//       'Sorry, there are no images matching your search query. Please try again.',
-//     );
-//   }
-// }
-
-// async function onShowMore() {
-//   API.increasePage();
-
-//   if (API.totalHits <= 20) {
-//     chekEndOfTotalHits();
-//     return;
-//   }
-
-//   const result = await API.fetchDataFromPixabay();
-
-//   rewrightMarkup(result);
-//   scrollAfterShowMore();
-
-//   API.lastTotalHits();
-//   lightbox.refresh();
-//   Notiflix.Notify.success(`Hooray! We found ${API.totalHits} images.`);
-// }
-
-// function rewrightMarkup(markup) {
-//   refs.gallery.insertAdjacentHTML('beforeend', imgTemplate(markup));
-// }
-
-// function resetMarkup() {
-//   refs.gallery.innerHTML = '';
-// }
-
-// function hideShowMoreBtn() {
-//   refs.showMoreBtn.classList.add('is-hidden');
-// }
-
-// function showShowMoreBtn() {
-//   refs.showMoreBtn.classList.remove('is-hidden');
-// }
-
-// function chekInputNotEmpty() {
-//   Notiflix.Notify.failure('Please enter something in search field');
-
-//   hideShowMoreBtn();
-//   resetMarkup();
-// }
-
-// function chekEndOfTotalHits() {
-//   hideShowMoreBtn();
-//   Notiflix.Notify.info("We're sorry, but you've reached the end of search results");
-// }
-
-// function scrollAfterShowMore() {
-//   let verticalParams = 0;
-
-//   const intervalId = setInterval(() => {
-//     window.scrollBy(0, verticalParams);
-//     verticalParams += 1;
-
-//     if (verticalParams === 20) {
-//       clearInterval(intervalId);
-//     }
-//   }, 20);
-// }
--- src/pixabay-service.js
+++ src/pixabay-service.js
@@ -18,15 +18,6 @@ export default class PixabayService {
     return response.data;
   }
 
-  // fetchImages() {
-  //   return axios(
-  //     `https://${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`,
-  //   ).then(resalt => {
-  //   this.incrementPage();
-  //     return resalt.data;
-  //   });
-  // }
-
   incrementPage() {
     this.page += 1;
   }
@@ -43,50 +34,3 @@ export default class PixabayService {
     this.searchQuery = newQuery;
   }
 }
-
-// ===========================================
-
-// <!-- другое решение -->
-
-// const axios = require('axios');
-
-// export default class fetchByName {
-//   constructor() {
-//     this.page = 1;
-//     this.value = '';
-//     this.totalHits = null;
-//   }
-
-//   async fetchDataFromPixabay() {
-//     const BASIC_URL = 'https://pixabay.com/api/';
-//     const URL_KEY = '27491593-aa922f21d022df769349f5779';
-//     const queryString = `q=${this.value}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
-
-//     const response = await axios.get(`${BASIC_URL}?key=${URL_KEY}&${queryString}`);
-
-//     if (!response.data.total) {
-//       throw new Error('error');
-//     }
-//     return response;
-//   }
-
-//   queryValue(newValue) {
-//     this.value = newValue;
-//   }
-
-//   increasePage() {
-//     this.page += 1;
-//   }
-
-//   resetPage() {
-//     this.page = 1;
-//   }
-
-//   setTotalHits(hits) {
-//     this.totalHits = hits;
-//   }
-
-//   lastTotalHits() {
-//     this.totalHits -= 40;
-//   }
-// }
